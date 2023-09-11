3025a541fe82ca2f4ab51f528a65ad3e5f0868c0
Joshua Omobola <omobolathejoshua@gmail.com>
2023-09-11 14:39:02 +0100

    feat: add lenis smooth scroll

--- app.vue
+++ app.vue
@@ -1,7 +1,33 @@
+<script setup>
+import { ref, inject, watch, onMounted } from 'vue';
+
+const scrollState = inject('scrollState');
+const lenisRef = ref(null);
+const lenisOptions = ref({
+  duration: .35,
+  direction: 'Horizontal',
+  touchMultiplier: 20,
+  infinite: false
+})
+
+watch(scrollState, async (val) => {
+  console.log('scrollState', val)
+})
+
+const scrollEmitter = (val) => {
+  console.log('scrollEmitter', val)
+}
+
+onMounted(() => {
+  console.log('ref', lenisRef.value)
+})
+</script>
 <template>
-  <div class="py-5 bg-black">
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </div>
+  <lenis id="test" ref="lenisRef" :options="lenisOptions" @scroll="scrollEmitter">
+    <div class="py-5 bg-black">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+  </lenis>
 </template>
