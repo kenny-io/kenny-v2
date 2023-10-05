7b6e13e6f31f77ba2d3325893062886d28ac2f26
kenny-io <ekeneeze3@gmail.com>
2023-10-06 02:11:49 +0400

    update keys

--- server/api/mailchimp.js
+++ server/api/mailchimp.js
@@ -17,7 +17,7 @@ export default defineEventHandler(async (event) => {
   });
   try {
     const response = await mailchimp.lists.addListMember(
-      runtimeConfig.MAILCHIMP_LIST_ID,
+      runtimeConfig.MAILCHIMP_AUDIENCE_ID,
       {
         email_address: email,
         status: 'subscribed',
@@ -34,6 +34,49 @@ export default defineEventHandler(async (event) => {
   } catch (err) {
     result = { message: err.response.body.title, status: err.status };
   }
+
+  // try {
+  //   const response = await fetch(
+  //     `https://${runtimeConfig.MAILCHIMP_SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${runtimeConfig.MAILCHIMP_AUDIENCE_ID}/members`,
+  //     {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         email_address: email,
+  //         merge_fields: {
+  //           FNAME: first_name,
+  //         },
+  //         status: 'subscribed',
+  //         tags: ['newsletter', 'website'],
+  //       }),
+  //       headers: {
+  //         Authorization: `apikey ${runtimeConfig.MAILCHIMP_API_KEY}`,
+  //         'Content-Type': 'application/json',
+  //       },
+  //     }
+  //   ).then((res) => res.json());
+
+  //   result = {
+  //     message: `Email ${response.email_address} subscribed to Mailchimp`,
+  //     status: 200,
+  //   };
+
+  //   if (response.status === 400) {
+  //     return {
+  //       statusCode: 400,
+  //       body: JSON.stringify(response),
+  //     };
+  //   }
+  //   // return success
+
+  //   return result;
+  // } catch (error) {
+  //   console.log(error);
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ msg: error.message }),
+  //   };
+  // }
+
   if (result.status !== 200)
     throw createError({
       statusCode: result.status,
